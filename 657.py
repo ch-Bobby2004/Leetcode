0a3a1b780f615971462637ffb355b186bd676614
Bobby <choudharybobby14nov@gmail.com>
2026-04-05 02:22:21 -0700

    Added new files

---
+++ 657.py
@@ -0,0 +1,8 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        return moves.count('U') == moves.count('D') and \
+               moves.count('L') == moves.count('R')
+    
+
+# Time: O(n)
+# Space: O(1)
