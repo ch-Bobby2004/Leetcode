34663d280f14ae4cb57e5cbb582750de3e0c5eb1
Bobby <choudharybobby14nov@gmail.com>
2026-04-09 02:59:15 -0700

    Added new files

---
+++ 1633.sql
@@ -0,0 +1,7 @@
+SELECT 
+    contest_id,
+    ROUND(COUNT(user_id) * 100.0 / 
+          (SELECT COUNT(DISTINCT user_id) FROM Register), 2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id ASC;
---
+++ 3653.py
@@ -0,0 +1,12 @@
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        MOD = 10**9 + 7
+        for l, r, k, v in queries:
+            idx = l
+            while idx <= r:
+                nums[idx] = (nums[idx] * v) % MOD
+                idx += k
+        ans = 0
+        for x in nums:
+            ans ^= x
+        return ans
