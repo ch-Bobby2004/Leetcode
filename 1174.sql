fb2ac41423d6f859bef8edc0749a67ecd403b940
Bobby <choudharybobby14nov@gmail.com>
2026-04-10 09:25:48 -0700

    Added new files

---
+++ 1174.sql
@@ -0,0 +1,17 @@
+WITH first_orders AS (
+    SELECT 
+        customer_id,
+        MIN(order_date) AS first_date
+    FROM Delivery
+    GROUP BY customer_id
+)
+
+SELECT 
+    ROUND(
+        SUM(d.order_date = d.customer_pref_delivery_date) / COUNT(*) * 100,
+        2
+    ) AS immediate_percentage
+FROM Delivery d
+JOIN first_orders f
+ON d.customer_id = f.customer_id
+AND d.order_date = f.first_date;
