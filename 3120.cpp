3dce107f08089f0f849675026d98330f1833548e
Bobby <choudharybobby14nov@gmail.com>
2026-05-26 05:33:57 -0700

    Added new files

---
+++ 3120.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        vector<bool> lower(26, false);
+        vector<bool> upper(26, false);
+
+        for (char ch : word) {
+            if (islower(ch))
+                lower[ch - 'a'] = true;
+            else
+                upper[ch - 'A'] = true;
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] && upper[i])
+                ans++;
+        }
+
+        return ans;
+    }
+};
